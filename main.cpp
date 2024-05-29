7529bec92976f24e67b5092b3ed6f00fc13e1c15
smh <smh>
2024-05-29 16:14:38 +0200

    dodane nawiasow

--- main.cpp
+++ main.cpp
@@ -1,48 +1,50 @@
 #include iostream
 using namespace std;
  
-void swap(int a, int b)
-int temp = a;
-a = b;
-b = temp;
+void swap(int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
  
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
  
-int partition(int arr, int low, int high)
-int pivot = arrhigh;
-int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot)
+            i++;
+        swap(arr[i], arr[j]);
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
  
-for (int j = low; j high; j++)
-if (arrj pivot)
-i++;
-swap(arri, arrj);
-swap(arri + 1, arrhigh);
-return i + 1;
+void quickSort(int* arr, int low, int high) {
+    if (low high)
+        int pi = partition(arr, low, high);
  
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+}
  
-void quickSort(int arr, int low, int high)
-if (low high)
-int pi = partition(arr, low, high);
+void printArray(int* arr, int size) {
+    for (int i = 0; i size; i++)
+        cout arri ;
+        cout endl;
+}
  
-quickSort(arr, low, pi - 1);
-quickSort(arr, pi + 1, high);
+int main() {
+    int arr = [10, 1000, 8, 9, 1, 5];
+    int n = sizeof(arr) / sizeof(arr0);
  
- 
-void printArray(int arr, int size)
-for (int i = 0; i size; i++)
-cout arri ;
-cout endl;
- 
- 
-int main()
-int arr = 10, 1000, 8, 9, 1, 5;
-int n = sizeof(arr) / sizeof(arr0);
- 
-cout nieposortowany: ;
-printArray(arr, n);
+    cout<<"nieposortowany: ";
+    printArray(arr, n);
  
 quickSort(arr, 0, n - 1);
  
-cout posortowany: ;
+cout<<"posortowany: ";
 printArray(arr, n);
  
 return 0;
+}
